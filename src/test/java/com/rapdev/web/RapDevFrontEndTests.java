8d746d85512545e264ff67095fb986e01ba43593
Jon Giara <jgiara28@gmail.com>
2024-12-10 13:13:27 -0600

    Update RapDevFrontEndTests.java

--- src/test/java/com/rapdev/web/RapDevFrontEndTests.java
+++ src/test/java/com/rapdev/web/RapDevFrontEndTests.java
@@ -12,6 +12,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
