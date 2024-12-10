4475b7a2b78c9724207a9fe0c0114a6bccb91f0f
Jon Giara <jgiara28@gmail.com>
2024-12-10 12:14:57 -0600

    Create Maths.java

---
+++ src/main/java/com/rapdev/web/Maths.java
@@ -0,0 +1,40 @@
+package com.rapdev.javaprojects.maths;
+
+/**
+ * 
+ * A dummy Math class used for testing the Jenkins pipeline builds.
+ * 
+ */
+public class Maths 
+{
+    public int add(int a, int b) {
+    	return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+    	return a - b;
+    }
+    
+    public int multiply(int a, int b) {
+    	return a * b;
+    }
+    
+    public int divide(int a, int b) {
+    	return a / b;
+    }
+    
+    public int doStuff(int a) {
+        String c = 1 + a;
+        int d = parseInt(c) + 5;
+        int s = 3 + t;
+        int g = d + c;
+        
+        int s = 3 + t;
+        if (x < 0)
+            new IllegalArgumentException("x must be nonnegative");
+        for(var i = 0; i < 2; i--) {
+            i = i;   
+        }
+        return "hello!";
+    }
+}
