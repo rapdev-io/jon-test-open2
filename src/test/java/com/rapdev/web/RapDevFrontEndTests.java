839840bf25340e1d62e001064b8df1b0c7a8a372
Jon Giara <jgiara28@gmail.com>
2024-12-10 13:12:24 -0600

    Update RapDevFrontEndTests.java

--- src/test/java/com/rapdev/web/RapDevFrontEndTests.java
+++ src/test/java/com/rapdev/web/RapDevFrontEndTests.java
@@ -23,7 +23,7 @@ public class RapDevFrontEndTests {
     @Test
     public void loginScreen() {
         Maths testClass = new Maths();
-        assertTrue(testClass.add(1, 1).equals(2));
+        assertTrue(testClass.add(1, 1) == 2);
     }
     
     @Test
