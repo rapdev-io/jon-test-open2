6a17f15bb0d588fedaa48279a5ecb2b64a960ad0
Jon Giara <jgiara28@gmail.com>
2025-04-21 09:33:27 -0500

    Update RapDevFrontEndTests.java

--- src/test/java/com/rapdev/web/RapDevFrontEndTests.java
+++ src/test/java/com/rapdev/web/RapDevFrontEndTests.java
@@ -1,4 +1,4 @@
-package com.rapdev.javaprojects.maths;
+//package com.rapdev.javaprojects.maths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
