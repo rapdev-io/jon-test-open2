dccf298cb33f53e7c097ca29b51a53e3d20a1f54
Jon Giara <jgiara28@gmail.com>
2025-04-21 12:06:16 -0500

    Update and rename MathTest.java to MathsTest.java

--- src/test/java/com/rapdev/web/MathsTest.java
+++ src/test/java/com/rapdev/web/MathsTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class MathTest {
+public class MathsTest {
     
     @Test
     public void loginScreen() {
